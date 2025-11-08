b52534115a9e7676d7c49ec43d51d9d286959ee4
ladyinredderzkuu3 <mwineesref@hotmail.com>
2025-11-08 07:12:31 -0600

    Update MongoDB connection string to use environment variables for improved security.

--- app.js
+++ app.js
@@ -6,12 +6,13 @@ const userRoutes = require('./routes/userRoutes');
 const app = express();
 const PORT = process.env.PORT || 3000;
 const DB_NAME = process.env.DB_NAME || 'tech-evangelismus';
+const DB_HOST = process.env.DB_HOST || 'localhost';
 
 app.use(cors());
 app.use(express.json());
 
 // MongoDB Verbindung
-mongoose.connect(`mongodb://localhost:27017/${DB_NAME}`, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(`mongodb://${DB_HOST}:27017/${DB_NAME}`, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('Verbindung zu MongoDB hergestellt.'))
   .catch(err => console.error('Mongoose-Verbindungsfehler:', err));
 
