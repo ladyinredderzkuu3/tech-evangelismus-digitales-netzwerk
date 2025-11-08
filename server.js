9c44d6d2a5145153cfcbe35ed05e5aa5a6c1ca7f
ladyinredderzkuu3 <mwineesref@hotmail.com>
2025-11-08 07:11:03 -0600

    Refactor: Replace deprecated body-parser with express.json() middleware

--- server.js
+++ server.js
@@ -1,12 +1,11 @@
 const express = require('express');
-const bodyParser = require('body-parser');
 const cors = require('cors');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(cors());
-app.use(bodyParser.json());
+app.use(express.json());
 
 app.get('/', (req, res) => {
   res.send('Welcome to the Tech Evangelism Digital Network!');
