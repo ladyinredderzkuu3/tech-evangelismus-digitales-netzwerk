8eb5c6a38af120d37f3524a47163bfc6976772f6
ladyinredderzkuu3 <mwineesref@hotmail.com>
2025-11-08 07:11:51 -0600

    Update MongoDB connection string to use environment variable for database name.

--- app.js
+++ app.js
@@ -5,12 +5,13 @@ const userRoutes = require('./routes/userRoutes');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+const DB_NAME = process.env.DB_NAME || 'tech-evangelismus';
 
 app.use(cors());
 app.use(express.json());
 
 // MongoDB Verbindung
-mongoose.connect('mongodb://localhost:27017/tech-evangelismus', { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(`mongodb://localhost:27017/${DB_NAME}`, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('Verbindung zu MongoDB hergestellt.'))
   .catch(err => console.error('Mongoose-Verbindungsfehler:', err));
 
