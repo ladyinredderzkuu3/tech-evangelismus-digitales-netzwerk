82b3dbdcd99fb93867307a1c43d26a755bbc345f
ladyinredderzkuu3 <mwineesref@hotmail.com>
2025-11-08 07:11:12 -0600

    Update welcome message for clarity.

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.use(cors());
 app.use(express.json());
 
 app.get('/', (req, res) => {
-  res.send('Welcome to the Tech Evangelism Digital Network!');
+  res.send('Welcome to the Tech Evangelism Network!');
 });
 
 app.listen(PORT, () => {
